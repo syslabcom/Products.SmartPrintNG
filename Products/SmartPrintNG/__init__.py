80b01bff1ec944c9cf33d2b87013fc06d6ebb96c
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2009-06-25 17:49:46 +0000

    Added support for l18n

--- Products/SmartPrintNG/__init__.py
+++ Products/SmartPrintNG/__init__.py
@@ -5,6 +5,9 @@
 # (C) 2007, ZOPYX Ltd & Co. KG, Tuebingen, Germany
 ##########################################################################
 
+# Import "MessageFactory" to create messages in the smartprintng domain
+from zope.i18nmessageid import MessageFactory
+SPNGMessageFactory = MessageFactory('smartprintng')
 
 from Products.CMFCore.DirectoryView import registerDirectory
 
